b58869966e7b8c29a5f8a1f7972e35cb1874edcc
Sampada-VS <samssvv1@gmail.com>
2020-08-05 17:12:28 +0530

    Adding Employee wage using function

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -8,10 +8,7 @@ MAX_HOURS=100
 total_emp_hrs=0
 total_working_days=0
 
-while [[ $total_emp_hrs -lt $MAX_HOURS && $total_working_days -lt $NUM_OF_WORKI>
-do
-        ((total_working_days++))
-        emp_check=$((RANDOM%3))
+function getEmpHours(){
         case $emp_check in
                 $IS_PART_TIME)
                         emp_hours=4
@@ -23,6 +20,14 @@ do
                         emp_hours=0
                         ;;
         esac
-        total_emp_hrs=$(($total_emp_hrs+$emp_hours))
+        echo $emp_hours
+}
+
+while [[ $total_emp_hrs -lt $MAX_HOURS && $total_working_days -lt $NUM_OF_WORKI>
+do
+        ((total_working_days++))
+        emp_check=$((RANDOM%3))
+        emp_working_hours=$( getEmpHours $emp_check )
+        total_emp_hrs=$(($total_emp_hrs+$emp_working_hours))
 done
 emp_wage=$(($total_emp_hrs*$RATE_PER_HOUR))
