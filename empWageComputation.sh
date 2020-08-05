703c49ffeb5fbadffcbf3b1f0a1678cb1e0ce2b6
Sampada-VS <samssvv1@gmail.com>
2020-08-05 16:29:20 +0530

    Adding employee daily wage for full and part time

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,12 +1,16 @@
 #!/bin/bash -x
-IS_PRESENT=1
-emp_check=$((RANDOM%2))
+IS_PART_TIME=1
+IS_FULL_TIME=2
+RATE_PER_HOUR=20
+emp_check=$((RANDOM%3))
 
-if [ $IS_PRESENT -eq $emp_check ]
+if [ $IS_PART_TIME -eq $emp_check ]
+then
+        emp_hours=4
+elif [ $IS_FULL_TIME -eq $emp_check ]
 then
         emp_hours=8
-        RATE_PER_HOUR=20
-        emp_daily_wage=$(($RATE_PER_HOUR*$emp_hours))
 else
-        emp_daily_wage=0
+        emp_hours=0
 fi
+emp_daily_wage=$(($RATE_PER_HOUR*$emp_hours))
