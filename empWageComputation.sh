d2701c2943901ae066ae58d1000ab978907f1b7e
Sampada-VS <samssvv1@gmail.com>
2020-08-05 17:28:11 +0530

    Adding Employee daily and total wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -8,6 +8,12 @@ MAX_HOURS=100
 total_emp_hrs=0
 total_working_days=0
 
+function getDailyWage(){
+        local emp_hrs=$1
+        emp_salary=$(($emp_hrs*$RATE_PER_HOUR))
+        echo $emp_salary
+}
+
 function getEmpHours(){
         case $emp_check in
                 $IS_PART_TIME)
@@ -27,7 +33,9 @@ while [[ $total_emp_hrs -lt $MAX_HOURS && $total_working_days -lt $NUM_OF_WORKI>
 do
         ((total_working_days++))
         emp_check=$((RANDOM%3))
-        emp_working_hours=$( getEmpHours $emp_check )
-        total_emp_hrs=$(($total_emp_hrs+$emp_working_hours))
+        emp_hours=$( getEmpHours $emp_check )
+        total_emp_hrs=$(($total_emp_hrs+$emp_hours))
+        dailyWage[total_working_days]="$( getDailyWage $emp_hours )"
 done
-emp_wage=$(($total_emp_hrs*$RATE_PER_HOUR))
+emp_wage=$( getDailyWage $total_emp_hrs )
+echo "Daily Wage " ${dailyWage[@]}
