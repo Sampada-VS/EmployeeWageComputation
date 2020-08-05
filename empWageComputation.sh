6d4d7e6d7938553643ee2f9723628f906d04848a
Sampada-VS <samssvv1@gmail.com>
2020-08-06 03:17:40 +0530

    Adding feature-Employee wage using function

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -9,25 +9,25 @@ total_emp_hrs=0
 total_working_days=0
 
 function getEmpHours(){
-        case $emp_check in
-                $IS_PART_TIME)
-                        emp_hours=4
-                        ;;
-                $IS_FULL_TIME)
-                        emp_hours=8
-                        ;;
-                *)
-                        emp_hours=0
-                        ;;
-        esac
-        echo $emp_hours
+	case $emp_check in
+		$IS_PART_TIME)
+			emp_hours=4
+			;;
+		$IS_FULL_TIME)
+			emp_hours=8
+			;;
+		*)
+			emp_hours=0
+			;;
+	esac
+	echo $emp_hours
 }
 
-while [[ $total_emp_hrs -lt $MAX_HOURS && $total_working_days -lt $NUM_OF_WORKI>
+while [[ $total_emp_hrs -lt $MAX_HOURS && $total_working_days -lt $NUM_OF_WORKING_DAYS ]]
 do
-        ((total_working_days++))
-        emp_check=$((RANDOM%3))
-        emp_working_hours=$( getEmpHours $emp_check )
-        total_emp_hrs=$(($total_emp_hrs+$emp_working_hours))
+	((total_working_days++))
+	emp_check=$((RANDOM%3))
+	emp_working_hours=$( getEmpHours $emp_check )
+	total_emp_hrs=$(($total_emp_hrs+$emp_working_hours))
 done
 emp_wage=$(($total_emp_hrs*$RATE_PER_HOUR))
