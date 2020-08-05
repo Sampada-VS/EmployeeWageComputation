1fedb7a87c31d8b23b2c35e77ca5c0b8129c7367
Sampada-VS <samssvv1@gmail.com>
2020-08-05 16:43:20 +0530

    Adding employee daily wage for full,part time using case

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,13 +4,15 @@ IS_FULL_TIME=2
 RATE_PER_HOUR=20
 emp_check=$((RANDOM%3))
 
-if [ $IS_PART_TIME -eq $emp_check ]
-then
-        emp_hours=4
-elif [ $IS_FULL_TIME -eq $emp_check ]
-then
-        emp_hours=8
-else
-        emp_hours=0
-fi
+case $emp_check in
+        $IS_PART_TIME)
+                emp_hours=4
+                ;;
+        $IS_FULL_TIME)
+                emp_hours=8
+                ;;
+        *)
+                emp_hours=0
+                ;;
+esac
 emp_daily_wage=$(($RATE_PER_HOUR*$emp_hours))
